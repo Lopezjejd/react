d87e5cbca66d9865334c2bb7c735d3aed64ffaa3
juan manuel <isaacesgaysupergay@gmail.com>
2025-09-24 22:23:03 -0500

    hero done

--- projects/mi-portfolio/src/components/Hero.tsx
+++ projects/mi-portfolio/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import profilePic  from '../assets/images/perfil.webp';
 import SocialLinks from './SocialLinks';
 export function Hero() {
     return (
-        <section className="flex flex-col items-center justify-center text-center gap-4 h-[80vh] px-4">
+        <section className="flex flex-col lg:flex-row items-center justify-center text-center gap-4 h-[80vh] px-4">
          <div className="max-w-2xl">
             <img src={profilePic} alt="imagen de un programador web"
             className='w-1/2 rounded-full m-auto' />
@@ -15,15 +15,17 @@ export function Hero() {
           <p className="text-2xl leading-snug text-white max-w-xs text-left m-auto mb-5
           font-bold ">
              <span className="block">Hi, I’m López</span>
-             <span className="block relative bg-principal p-1 overflow-hidden px-2 w-fit after:content-[''] after:block after:w-2  after:left-0 after:absolute after:top-[-10px] 
-                after:h-10 after:bg-white/60 after:rotate-35 after:brightness-1250
-             ">Frontend Developer</span>
-             <span className="block text-xl">Passionate about creating modern and dynamic web experiences</span>
+             <span className="block relative bg-principal p-1 overflow-hidden px-2 w-fit 
+             ">Frontend Developer <span className='block w-2 left-0 absolute top-[-20px] h-20 bg-white/30 rotate-35 animate-brillo' ></span></span>
+             <span className="block font-normal text-[1.1rem]">Passionate about creating modern and dynamic web experiences</span>
              </p>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+         <div className='flex flex-col gap-4 flex-wrap pr-20 '>
+            <button className='w-[60%] cursor-pointer hover:rounded-[5px] transition-all py-1 m-auto font-semibold bg-principal'>Technologies</button>
+            <button className='w-[60%] cursor-pointer hover:rounded-[5px] transition-all py-1 m-auto font-semibold bg-principal'>Projects</button>
+            <button className='w-[60%] cursor-pointer hover:rounded-[5px] transition-all py-1 m-auto font-semibold bg-principal'>Skills</button>
+            <button className='w-[60%] cursor-pointer hover:rounded-[5px] transition-all py-1 m-auto font-semibold bg-principal'>About Me</button>
+
+         </div>
          </div>
         </section>
     )
